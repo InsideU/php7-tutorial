abded5c2220c493ec20f5148486c9dbbbc5ee5a2
Umang Singh <43104796+InsideU@users.noreply.github.com>
2019-06-03 16:16:58 +0000

    Update auth.php

--- auth.php
+++ auth.php
@@ -13,12 +13,12 @@ if($conn->connect_error){   // never miss dollar sign with conn here
 else{
 	echo "\r \nconnection established \r \n";
 }
-$auth_username=$_POST["username"];
-$auth_password=$_POST["password"];
+$auth_username=$_POST["username"];  // extract the username from the post request sent from the form created for input
+$auth_password=$_POST["password"]; // extract the password from the post request send form the form careted for iput
 $sql="select * from STUDENTS where firstname='$auth_username' and lastname='$auth_password'";
-$result = $conn->query($sql);
-if ($result->num_rows > 0){
-	echo " \r\n User Verified and Access Granted \r\n";
+$result = $conn->query($sql);		
+if ($result->num_rows > 0){		// if there is an entry then number of rows will always be greater than 0
+	echo " \r\n User Verified and Access Granted \r\n"; // if there is a row then there is an entry thus user verified
 }
 else{
    echo	"\r\n Access Denied \r\n";
