62bc2e617b49036e79a2c70a355fe94eb9520764
tichkoo27 <56758175+tichkoo27@users.noreply.github.com>
2019-10-31 22:13:43 +0530

    Update auth.php

--- auth.php
+++ auth.php
@@ -13,8 +13,8 @@ if($conn->connect_error){   // never miss dollar sign with conn here
 else{
 	echo "\r \nconnection established \r \n";
 }
-$auth_username=$_POST["username"];  // extract the username from the post request sent from the form created for input
-$auth_password=$_POST["password"]; // extract the password from the post request send form the form careted for iput
+$auth_username=$_POST["username"];  // extract the username from the post request sent form created for input
+$auth_password=$_POST["password"]; // extract the password from the post request and send it to the user form 
 $sql="select * from STUDENTS where firstname='$auth_username' and lastname='$auth_password'";
 $result = $conn->query($sql);		
 if ($result->num_rows > 0){		// if there is an entry then number of rows will always be greater than 0
